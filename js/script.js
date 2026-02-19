b623f0e690fa0b4fc835519e775eb9b192bfa304
Aldren-1 <restauroaldrin@gmail.com>
2026-02-19 15:13:14 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -15,27 +15,16 @@ const themeToggle = document.getElementById("themeToggle");
 themeToggle.addEventListener("click",function(){
 document.body.classList.toggle("dark-mode");
 });
+themeToggle.addEventListener("click", function() {
+  if (this.textContent === "Toggle Dark Mode") {
+    this.textContent = "Toggle Light Mode"; 
+  } else {
+    this.textContent = "Toggle Dark Mode"; 
+  }
+});
 
 const toggleSkills = document.getElementById("toggleSkills");
 const skillsSection = document.getElementById("skillsSection");
 toggleSkills.addEventListener("click",function(){
 skillsSection.classList.toggle("hidden");
-});
-
-const toggleOtherSkills = document.getElementById("toggleOtherSkills");
-const OtherskillsSection = document.getElementById("OtherskillsSection");
-toggleOtherSkills.addEventListener("click",function(){
-OtherskillsSection.classList.toggle("hidden");
-});
-
-const toggleEduc = document.getElementById("toggleEduc");
-const EducSection = document.getElementById("EducSection");
-toggleEduc.addEventListener("click",function(){
-EducSection.classList.toggle("hidden");
-});
-
-const toggleContacts = document.getElementById("toggleContacts");
-const ContactsSection = document.getElementById("ContactsSection");
-toggleContacts.addEventListener("click",function(){
-ContactsSection.classList.toggle("hidden");
 });
