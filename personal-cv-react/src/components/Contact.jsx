f4848963e115a8aa966ea6efba51c6029a642380
IKAB005 <Bris.Ian2005@gmail.com>
2026-04-16 15:50:43 +0800

    WSAT-M7: Connected React to PHP API

--- personal-cv-react/src/components/Contact.jsx
+++ personal-cv-react/src/components/Contact.jsx
@@ -1,29 +1,34 @@
 import { useState } from "react";
 function Contact() {
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-    return (
-        <section className="card">
-                <h2>Contact Me</h2>
-                <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    />
-                    <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <textarea placeholder="Message"></textarea>
-                    <button type="submit" id="submitBtn">Send</button>
-                </form>
-        </section>
-    )
-}
+    const [name, setName] = useState("");
 function handleSubmit(e) {
     e.preventDefault();
-    alert(`Thank you ${name}!`);
+        fetch("http://localhost/cv-api/process.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+                },
+                    body: JSON.stringify({ name: name })
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                    if (data.message) {
+                    alert(data.message);
+                    } else {
+                    alert("Unexpected error occurred.");
+                    }
+                });
+            }
+            return (
+                <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+            />
+            <button type="submit">Send</button>
+        </form>
+    );
 }
 export default Contact;
